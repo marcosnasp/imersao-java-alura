57f8ea97c4aafd3a9f6c14e3b0bfa63fd372ddce
Marcos <marcosnasp@gmail.com>
2022-07-19 18:41:49 -0300

    Aula 02 - Alura - Criando a edição de figurinhas

--- imdb-api-consumer/src/GeradoraDeFigurinhas.java
+++ imdb-api-consumer/src/GeradoraDeFigurinhas.java
@@ -1,9 +1,13 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
@@ -18,12 +22,14 @@ public class GeradoraDeFigurinhas {
         File diretorioEntradaFilme = new File("imdb-api-consumer\\entrada");
         verificarExistenciaDiretorioEntrada(diretorioEntradaFilme);
 
-        File arquivoEntradaFilme = new File("imdb-api-consumer\\entrada\\filme.jpg");
+        File arquivoEntradaFilme = new File("imdb-api-consumer\\entrada\\filme-maior.jpg");
         verificarArquivoEntradaExiste(arquivoEntradaFilme);
 
         File diretorioSaida = criarDiretorioSaida("imdb-api-consumer\\saida");
         File arquivoSaida = new File(diretorioSaida, "figurinha.png");
-        BufferedImage imagemOriginal = ImageIO.read(arquivoEntradaFilme);
+
+        InputStream inputStream = new FileInputStream(arquivoEntradaFilme);
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // criar uma nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
@@ -36,8 +42,14 @@ public class GeradoraDeFigurinhas {
         graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
-        ImageIO.write(novaImagem, "png", arquivoSaida);
         // escreva um texto na nova imagem
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
+        Font fontString = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        Color cor = Color.YELLOW;
+        graphics.setColor(cor);
+        graphics.setFont(fontString);
+
+        ImageIO.write(novaImagem, "png", arquivoSaida);
 
         // escrever a nova imagem no arquivo
 
